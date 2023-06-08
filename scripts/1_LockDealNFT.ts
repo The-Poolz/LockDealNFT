b5147bd05ff9669d67e916e690700cb6103571f0
Andrew Dmytrenko <dmitrenkoae3@gmail.com>
2023-06-08 12:49:25 +0300

    update deployment scripts

---
+++ scripts/1_LockDealNFT.ts
@@ -0,0 +1,15 @@
+import { deployed } from "../test/helper";
+import { LockDealNFT } from "../typechain-types/contracts/LockDealNFT";
+
+async function main() {
+  const vaultManager = "0x240e127c67904984f8F42e49084e2Cc1b875D47D"//TODO: replace with your vaultManager address
+  const lockDealNFT: LockDealNFT = await deployed("LockDealNFT", vaultManager);
+  console.log(
+    `Contract deployed to ${lockDealNFT.address} with vaultManager ${vaultManager}`
+  );
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
---
+++ scripts/2_DealProvider.ts
@@ -0,0 +1,15 @@
+import { deployed } from "../test/helper";
+import { DealProvider } from "../typechain-types/contracts/DealProvider";
+
+async function main() {
+  const lockDealNFT = "0xCcf41440a137299CB6af95114cb043Ce4e28679A"// TODO: replace with your lockDealNFT address
+  const dealProvider: DealProvider = await deployed("DealProvider", lockDealNFT);
+  console.log(
+    `Contract deployed to ${dealProvider.address} with lockDealNFT ${lockDealNFT}`
+  );
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
