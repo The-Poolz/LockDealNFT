6b3ce099c20aa2a2bcb65547fd38d6dfe043818e
Andrew Dmytrenko <dmitrenkoae3@gmail.com>
2023-06-08 12:55:04 +0300

    sonar fix

--- scripts/1_LockDealNFT.ts
+++ scripts/1_LockDealNFT.ts
@@ -2,7 +2,7 @@ import { deployed } from "../test/helper";
 import { LockDealNFT } from "../typechain-types/contracts/LockDealNFT";
 
 async function main() {
-  const vaultManager = "0x240e127c67904984f8F42e49084e2Cc1b875D47D"//TODO: replace with your vaultManager address
+  const vaultManager = "0x240e127c67904984f8F42e49084e2Cc1b875D47D"// replace with your vaultManager address
   const lockDealNFT: LockDealNFT = await deployed("LockDealNFT", vaultManager);
   console.log(
     `Contract deployed to ${lockDealNFT.address} with vaultManager ${vaultManager}`
