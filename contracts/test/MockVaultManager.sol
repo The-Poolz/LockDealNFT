bb02b61d1bf66ed451e7543c94a65b7c3ee5f51a
0xSolcroot <de.solcroot@gmail.com>
2023-06-15 21:45:12 +0900

    update MockVaultManager contract

--- contracts/test/MockVaultManager.sol
+++ contracts/test/MockVaultManager.sol
@@ -1,20 +1,26 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
 contract MockVaultManager {
+    address public tokenAddress;
+
     function depositByToken(
         address _tokenAddress,
         address from,
         uint _amount
     ) external returns (uint vaultId) {
-        // do nothing
+        tokenAddress = _tokenAddress;
+        IERC20(_tokenAddress).transferFrom(from, address(this), _amount);
+        vaultId = 0;
     }
 
     function withdrawByVaultId(
-        uint _vaultId,
+        uint /** _vaultId **/,
         address to,
         uint _amount
     ) external {
-        // do nothing
+        IERC20(tokenAddress).transfer(to, _amount);
     }
 }
