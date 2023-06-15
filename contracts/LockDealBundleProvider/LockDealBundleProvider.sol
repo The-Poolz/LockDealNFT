8f745e8388dfe7fea0cdd152eb12deaac48ead9b
0xSolcroot <de.solcroot@gmail.com>
2023-06-16 01:01:34 +0900

    remove the unused library

--- contracts/LockDealBundleProvider/LockDealBundleProvider.sol
+++ contracts/LockDealBundleProvider/LockDealBundleProvider.sol
@@ -4,7 +4,6 @@ pragma solidity ^0.8.0;
 import "./LockDealBundleProviderModifiers.sol";
 import "../Provider/ProviderModifiers.sol";
 import "../ProviderInterface/IProvider.sol";
-import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/token/ERC721/utils/ERC721Holder.sol";
 
 interface IProviderExtend {
@@ -17,8 +16,6 @@ contract LockDealBundleProvider is
     IProvider,
     ERC721Holder
 {
-    using SafeERC20 for IERC20;
-
     constructor(address nft) {
         require(
             nft != address(0x0),
