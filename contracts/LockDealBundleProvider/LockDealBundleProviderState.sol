fe2412943b9aba714437effb8b6812e42ec4d769
0xSolcroot <de.solcroot@gmail.com>
2023-06-20 01:07:44 +0900

    rename the state var

--- contracts/LockDealBundleProvider/LockDealBundleProviderState.sol
+++ contracts/LockDealBundleProvider/LockDealBundleProviderState.sol
@@ -6,5 +6,5 @@ import "../LockDealNFT/LockDealNFT.sol";
 /// @title LockDealBundleProviderState contract
 /// @notice Contains storage variables
 contract LockDealBundleProviderState {
-    mapping(uint256 => uint256) public poolIdToLockDealBundle;
+    mapping(uint256 => uint256) public bundlePoolIdToFirstPoolId;
 }
