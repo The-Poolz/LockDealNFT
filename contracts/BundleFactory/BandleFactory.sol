ba60e7ce3c7dd8ac807d69050b1e326f28b665f2
Stan Goldin <salty87@gmail.com>
2023-06-21 21:44:14 +0300

    fix proxyProvider

--- contracts/BundleFactory/BandleFactory.sol
+++ contracts/BundleFactory/BandleFactory.sol
@@ -8,10 +8,15 @@ import "../ProxyProvider/ProxyProvider.sol";
 
 contract BundleFactory is IERC721Receiver {
     LockDealBundleProvider public lockDealBundleProvider;
-
+    ProxyProvider public proxyProvider;
     LockDealNFT public lockDealNFT;
     mapping(address => uint256[]) public userToBundles;
-    constructor(address _nftContract,address _BundleContract, address _proxyProvider) {
+
+    constructor(
+        address _nftContract,
+        address _BundleContract,
+        address _proxyProvider
+    ) {
         require(_BundleContract != address(0x0), "invalid address");
         require(_nftContract != address(0x0), "invalid address");
         require(_proxyProvider != address(0x0), "invalid address");
@@ -26,12 +31,15 @@ contract BundleFactory is IERC721Receiver {
         uint256 poolId,
         bytes calldata
     ) external override returns (bytes4) {
-        require(validateToken(from,poolId),"invalid token");
+        require(validateToken(from, poolId), "invalid token");
         userToBundles[from].push(poolId);
         return IERC721Receiver.onERC721Received.selector;
     }
 
-    function validateToken(address user, uint256 poolId) internal view returns (bool valid) {
+    function validateToken(
+        address user,
+        uint256 poolId
+    ) internal view returns (bool valid) {
         uint256[] memory bundles = userToBundles[user];
         if (bundles.length == 0) return true;
         address newToken = lockDealNFT.getTokenAddress(poolId);
@@ -40,31 +48,62 @@ contract BundleFactory is IERC721Receiver {
     }
 
     function build() external {
-        uint256[] memory bundles = userToBundles[user];
+        uint256[] memory bundles = userToBundles[msg.sender];
         require(bundles.length > 1, "no bundles"); //need 2+ pools to build a bundle
         uint256 firstId = lockDealNFT.tokenIdCounter.current();
         uint256[] memory params = new uint256[](bundles.length);
         for (uint256 i = 0; i < bundles.length; i++) {
-            lockDealNFT.safeTransferFrom(address(this), address(proxyProvider), bundles[i]);
-            params[i] = bundles[i]; 
-            lockDealNFT.safeTransferFrom(address(this), address(lockDealBundleProvider), firstId + i);          
+            lockDealNFT.safeTransferFrom(
+                address(this),
+                address(proxyProvider),
+                bundles[i]
+            );
+            params[i] = bundles[i];
+            lockDealNFT.safeTransferFrom(
+                address(this),
+                address(lockDealBundleProvider),
+                firstId + i
+            );
         }
         address token = lockDealNFT.getTokenAddress(bundles[0]);
-        uint256 poolId = lockDealNFT.mint(msg.sender, token, msg.sender, 0, lockDealBundleProvider);
+        uint256 poolId = lockDealNFT.mint(
+            msg.sender,
+            token,
+            msg.sender,
+            0,
+            lockDealBundleProvider
+        );
         lockDealBundleProvider._registerPool(poolId, msg.sender, token, params);
+        userToBundles[msg.sender] = new uint256[](0);
     }
 
     function extract(uint256 poolid) external {
-        require(lockDealNFT.ownerOf(poolid) == address(this), "not in contract");
-        require(isOwner(msg.sender, poolid), "not owner");
+        require(
+            lockDealNFT.ownerOf(poolid) == address(this),
+            "not in contract"
+        );
+        RemoveIfOwner(msg.sender, poolid);
         lockDealNFT.safeTransferFrom(address(this), msg.sender, poolid);
     }
 
-    function isOwner(address user, uint256 poolId) internal view returns (bool valid) {
+    function RemoveIfOwner(
+        address user,
+        uint256 poolId
+    ) internal {
+        bool valid = false;
+        uint256 index = 0;
         uint256[] memory bundles = userToBundles[msg.sender];
         require(bundles.length > 0, "no bundles");
-        for (uint256 i = 0; i < bundles.length; i++) {
-            if (bundles[i] == poolId) return true;
-        }      
+        for (index = 0; index < bundles.length; index++) {
+            if (bundles[i] == poolId)
+            {
+                valid = true;
+                break;
+            }
+        }
+        require(valid, "not owner");
+        bundles[index] = bundles[bundles.length - 1];
+        bundles.pop();
+        userToBundles[msg.sender] = bundles;
     }
 }
