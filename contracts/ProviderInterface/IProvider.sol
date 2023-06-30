bd8fa2de58a1656f4204538b62a6a8bb7813fe3e
Andrew Dmytrenko <dmitrenkoae3@gmail.com>
2023-06-30 15:36:19 +0300

    remove import

--- contracts/ProviderInterface/IProvider.sol
+++ contracts/ProviderInterface/IProvider.sol
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "../DealProvider/IDealProvierEvents.sol";
-
 ///@dev Interface for the provider contract
 ///@notice This interface is used by the NFT contract to call the provider contract
 interface IProvider {
