9617d177814f07de56463f6d0f7a5e7e4ac7922a
Stan Goldin <salty87@gmail.com>
2023-07-09 14:13:58 +0300

    take from master

--- contracts/RefundProvider/RefundState.sol
+++ contracts/RefundProvider/RefundState.sol
@@ -1,15 +1,18 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "../CollateralProvider/CollateralProvider.sol";
+import "../LockProvider/LockDealProvider.sol";
 
 abstract contract RefundState is ProviderModifiers, IProvider {
-    CollateralProvider public collateralProvider;
+    LockDealProvider public lockProvider;
+    mapping(uint256 => address) public poolIdToProjectOwner;
 
-    function getParams(uint256 poolId) external view override returns (uint256[] memory params){
-        uint256 dataPoolId = poolId + 1;
-        if(lockDealNFT.exist(dataPoolId)) {
-            params = lockDealNFT.providerOf(dataPoolId).getParams(dataPoolId);
+    function getData(uint256 poolId) external view override returns (IDealProvierEvents.BasePoolInfo memory poolInfo, uint256[] memory params){
+        if (lockDealNFT.exist(poolId - 2)) {
+            (poolInfo, params) = IProvider(lockDealNFT.poolIdToProvider(poolId - 2)).getData(poolId - 2);
+            poolInfo.poolId = poolId;
+            if (lockDealNFT.exist(poolId))
+                poolInfo.owner = lockDealNFT.ownerOf(poolId);
         }
     }
 }
