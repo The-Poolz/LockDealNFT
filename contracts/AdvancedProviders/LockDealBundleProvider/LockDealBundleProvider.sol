3abb3971c429c4609383b042edbf988a1e8bbac4
Andrew Dmytrenko <dmitrenkoae3@gmail.com>
2023-07-13 12:59:18 +0300

    added registerPool function, refactoring

--- contracts/AdvancedProviders/LockDealBundleProvider/LockDealBundleProvider.sol
+++ contracts/AdvancedProviders/LockDealBundleProvider/LockDealBundleProvider.sol
@@ -2,15 +2,9 @@
 pragma solidity ^0.8.0;
 
 import "./LockDealBundleProviderState.sol";
-import "../../SimpleProviders/Provider/ProviderModifiers.sol";
 import "@openzeppelin/contracts/token/ERC721/utils/ERC721Holder.sol";
 
-contract LockDealBundleProvider is
-    LockDealBundleProviderState,
-    ProviderModifiers,
-    IProvider,
-    ERC721Holder
-{
+contract LockDealBundleProvider is LockDealBundleProviderState, ERC721Holder {
     constructor(address nft) {
         require(
             nft != address(0x0),
@@ -36,23 +30,31 @@ contract LockDealBundleProvider is
         uint256 totalAmount = _calcTotalAmount(providerParams);
         // create a new bundle pool owned by the owner
         poolId = lockDealNFT.mintAndTransfer(owner, token, msg.sender, totalAmount, address(this));
-
         uint256 lastSubPoolId;
         for (uint256 i; i < providerCount; ++i) {
             address provider = providers[i];
-            uint256[] memory params = providerParams[i];
-
             // check if the provider address is valid
             require(provider != address(lockDealNFT) && provider != address(this), "invalid provider address");
             // mint the NFT owned by the BunderDealProvider with 0 token transfer amount
-            lastSubPoolId = _createNewSubPool(address(this), provider, params);
+            lastSubPoolId = _createNewSubPool(address(this), provider, providerParams[i]);
         }
-
-        bundlePoolIdToLastSubPoolId[poolId] = lastSubPoolId;
+        uint256 [] memory bundleParams = new uint256[](1);
+        bundleParams[0] = lastSubPoolId;
+        _registerPool(poolId, bundleParams);
     }
 
-    function registerPool(uint256 poolId,uint256[] calldata params) external override onlyProvider {
-        // TODO: needs to be implemented
+    function registerPool(uint256 poolId, uint256[] calldata params) external override onlyProvider {
+        _registerPool(poolId, params);
+    }
+
+    ///@param params[0] = lastSubPoolId
+    function _registerPool(uint256 poolId, uint256[] memory params) internal {
+        uint256 lastSubPoolId = params[0];
+        require(poolId < lastSubPoolId, "poolId can't be greater than lastSubPoolId");
+        for(uint256 i = poolId + 1; i < lastSubPoolId; ++i) {
+            require(lockDealNFT.ownerOf(i) == address(this), "invalid owner of sub pool");
+        }
+        bundlePoolIdToLastSubPoolId[poolId] = params[0];
     }
 
     function _createNewSubPool(
@@ -102,29 +104,5 @@ contract LockDealBundleProvider is
 
         // finally, set the bundle provider state with the last sub pool Id
         bundlePoolIdToLastSubPoolId[newPoolId] = oldLastSubPoolId + (newPoolId - oldPoolId);
-    }
-
-    function getParams(uint256 poolId) public view override returns (uint256[] memory params) {
-        params = new uint256[](1);
-        params[0] = bundlePoolIdToLastSubPoolId[poolId]; //TODO this will change to the Last Pool Id
-    }
-
-    function getTotalRemainingAmount(uint256 poolId) public view returns (uint256 totalRemainingAmount) {
-        (address provider,,) = lockDealNFT.getData(poolId);
-        require(provider == address(this), "not bundle poolId");
-
-        uint256 lastSubPoolId = bundlePoolIdToLastSubPoolId[poolId];
-        for (uint256 i = poolId + 1; i <= lastSubPoolId; ++i) {
-            (,, uint256[] memory params) = lockDealNFT.getData(i);
-            totalRemainingAmount += params[0];  // leftAmount
-        }
-    }
-
-    function _calcRate(uint256 tokenAValue, uint256 tokenBValue) internal pure returns (uint256) {
-        return (tokenAValue * 1e18) / tokenBValue;
-    }
-
-    function _calcAmount(uint256 amount, uint256 rate) internal pure returns (uint256) {
-        return amount * 1e18 / rate;
     }
 }
--- contracts/AdvancedProviders/LockDealBundleProvider/LockDealBundleProviderState.sol
+++ contracts/AdvancedProviders/LockDealBundleProvider/LockDealBundleProviderState.sol
@@ -2,10 +2,11 @@
 pragma solidity ^0.8.0;
 
 import "../../LockDealNFT/LockDealNFT.sol";
+import "../../SimpleProviders/Provider/ProviderModifiers.sol";
 
 /// @title LockDealBundleProviderState contract
 /// @notice Contains storage variables
-contract LockDealBundleProviderState {
+abstract contract LockDealBundleProviderState is IProvider, ProviderModifiers {
     mapping(uint256 => uint256) public bundlePoolIdToLastSubPoolId;
 
     function _calcTotalAmount(uint256[][] calldata params) internal pure returns (uint256 totalAmount) {
@@ -13,5 +14,29 @@ contract LockDealBundleProviderState {
         for (uint256 i = 0; i < length; i++) {
             totalAmount += params[i][0];
         }
+    }
+
+    function getParams(uint256 poolId) public view override returns (uint256[] memory params) {
+        params = new uint256[](1);
+        params[0] = bundlePoolIdToLastSubPoolId[poolId]; //TODO this will change to the Last Pool Id
+    }
+
+    function getTotalRemainingAmount(uint256 poolId) public view returns (uint256 totalRemainingAmount) {
+        (address provider,,) = lockDealNFT.getData(poolId);
+        require(provider == address(this), "not bundle poolId");
+
+        uint256 lastSubPoolId = bundlePoolIdToLastSubPoolId[poolId];
+        for (uint256 i = poolId + 1; i <= lastSubPoolId; ++i) {
+            (,, uint256[] memory params) = lockDealNFT.getData(i);
+            totalRemainingAmount += params[0];  // leftAmount
+        }
+    }
+
+    function _calcRate(uint256 tokenAValue, uint256 tokenBValue) internal pure returns (uint256) {
+        return (tokenAValue * 1e18) / tokenBValue;
+    }
+
+    function _calcAmount(uint256 amount, uint256 rate) internal pure returns (uint256) {
+        return amount * 1e18 / rate;
     }
 }
