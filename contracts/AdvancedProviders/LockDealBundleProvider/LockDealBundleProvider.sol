5a54f375054e8c2e91b1bd712e5119af705759d0
Andrew Dmytrenko <dmitrenkoae3@gmail.com>
2023-07-13 13:08:41 +0300

    return params variable, added validParams check

--- contracts/AdvancedProviders/LockDealBundleProvider/LockDealBundleProvider.sol
+++ contracts/AdvancedProviders/LockDealBundleProvider/LockDealBundleProvider.sol
@@ -33,10 +33,12 @@ contract LockDealBundleProvider is LockDealBundleProviderState, ERC721Holder {
         uint256 lastSubPoolId;
         for (uint256 i; i < providerCount; ++i) {
             address provider = providers[i];
+            uint256[] memory params = providerParams[i];
+
             // check if the provider address is valid
             require(provider != address(lockDealNFT) && provider != address(this), "invalid provider address");
             // mint the NFT owned by the BunderDealProvider with 0 token transfer amount
-            lastSubPoolId = _createNewSubPool(address(this), provider, providerParams[i]);
+            lastSubPoolId = _createNewSubPool(address(this), provider, params);
         }
         uint256 [] memory bundleParams = new uint256[](1);
         bundleParams[0] = lastSubPoolId;
@@ -48,7 +50,7 @@ contract LockDealBundleProvider is LockDealBundleProviderState, ERC721Holder {
     }
 
     ///@param params[0] = lastSubPoolId
-    function _registerPool(uint256 poolId, uint256[] memory params) internal {
+    function _registerPool(uint256 poolId, uint256[] memory params) internal validParamsLength(params.length, 1) {
         uint256 lastSubPoolId = params[0];
         require(poolId < lastSubPoolId, "poolId can't be greater than lastSubPoolId");
         for(uint256 i = poolId + 1; i < lastSubPoolId; ++i) {
