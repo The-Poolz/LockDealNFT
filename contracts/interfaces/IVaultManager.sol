3a741982240173ddc71592e92c8e11fecfc9a55d
Stan Goldin <salty87@gmail.com>
2023-08-06 12:06:59 +0300

    fix miss type, add todo for IERC165

--- contracts/interfaces/IVaultManager.sol
+++ contracts/interfaces/IVaultManager.sol
@@ -4,6 +4,6 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/interfaces/IERC2981.sol";
 import "@poolzfinance/poolz-helper-v2/contracts/interfaces/IVaultManager.sol";
 
-interface c is IVaultManager, IERC2981
+interface IFullVault is IVaultManager, IERC2981
 {  
 }
