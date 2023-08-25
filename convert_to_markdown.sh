4dbf1c76c554effb1fee11936be8b071bee7f6f8
Stan Goldin <salty87@gmail.com>
2023-08-25 17:48:46 +0300

    fix convert try 2

--- convert_to_markdown.sh
+++ convert_to_markdown.sh
@@ -3,7 +3,6 @@
 input_file="$1"
 output_file="$2"
 
-echo "| Contract | Method | Min | Max | Avg | # calls | USD (avg) |" > "$output_file"
-echo "|----------|--------|-----|-----|-----|---------|-----------|" >> "$output_file"
-
-awk -F'|' '/^[[:space:]]*\|/{print $2, $3, $4, $5, $6, $7, $8}' "$input_file" >> "$output_file"
+awk -F'|' '
+    /^[[:space:]]*\|/{sub(/^\| /, ""); sub(/ \|\s*$/, ""); print}
+' "$input_file" > "$output_file"
