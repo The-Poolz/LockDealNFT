c772f7f2504e2cbf382a50b08f541484eea86b71
Stan Goldin <salty87@gmail.com>
2023-08-25 18:06:48 +0300

    fix loop

--- convert_to_markdown.sh
+++ convert_to_markdown.sh
@@ -5,18 +5,18 @@ input_file="gasReporterOutput.json"
 json_data=$(cat "$input_file")
 
 # Extract methods information
-methods_info=$(echo "$json_data" | jq -r '.info.methods')
+methods_info=$(echo "$json_data" | jq -r '.info.methods | to_entries[]')
 
 # Create Markdown table header
 markdown_table="| Contract | Method | Gas Data | # calls |\n"
 markdown_table+="|----------|--------|----------|---------|\n"
 
 # Loop through methods and add rows to the Markdown table
-while IFS= read -r method_info; do
-    contract=$(echo "$method_info" | jq -r '.contract')
-    method=$(echo "$method_info" | jq -r '.method')
-    gas_data=$(echo "$method_info" | jq -r '.gasData | join(", ")')
-    num_calls=$(echo "$method_info" | jq -r '.numberOfCalls')
+while IFS= read -r method_entry; do
+    contract=$(echo "$method_entry" | jq -r '.value.contract')
+    method=$(echo "$method_entry" | jq -r '.value.method')
+    gas_data=$(echo "$method_entry" | jq -r '.value.gasData | join(", ")')
+    num_calls=$(echo "$method_entry" | jq -r '.value.numberOfCalls')
     markdown_table+="| $contract | $method | $gas_data | $num_calls |\n"
 done <<< "$methods_info"
 
