a07266f4040c13229ac4b6cad52b0c399f2b276d
Stan Goldin <salty87@gmail.com>
2023-08-27 13:59:34 +0300

    add calculations

--- filterGasReport.js
+++ filterGasReport.js
@@ -1,19 +1,70 @@
 const fs = require('fs');
 
-// Read the existing report directly
-const rawReport = fs.readFileSync('gasReporterOutput.json', 'utf-8');
-const parsedReport = JSON.parse(rawReport);
+function calculateGasStatistics(gasData) {
+  const maxGas = Math.max(...gasData);
+  const minGas = Math.min(...gasData);
+  const avgGas = gasData.reduce((acc, val) => acc + val, 0) / gasData.length;
+  return { maxGas, minGas, avgGas };
+}
 
-let tableReadyData = {};
+try {
+  // Read the existing report directly
+  const rawReport = fs.readFileSync('gasReporterOutput.json', 'utf-8');
+  const parsedReport = JSON.parse(rawReport);
 
-// Your filtering logic here
-// For example:
+  // Check if parsedReport and info property exist
+  if (!parsedReport || !parsedReport.info) {
+    console.error("Error: Missing 'info' property in JSON report.");
+    process.exit(1);
+  }
 
-tableReadyData.methods = parsedReport.methods.filter(method => method.numberOfCalls > 0 && method.gasData.length > 0);
+  // Extract the 'info' object for easier reference
+  const reportInfo = parsedReport.info;
 
-tableReadyData.deployments = parsedReport.deployments.filter(
-  deployment => deployment.gasData !== undefined && deployment.gasData.length > 0,
-);
+  let tableReadyData = {
+    methods: [],
+    deployments: [],
+  };
 
-// Set this filtered data as an output variable for the next step
-console.log(`::set-output name=filtered_report::${JSON.stringify(tableReadyData)}`);
+  // Filter and aggregate method data
+  if (reportInfo.methods) {
+    tableReadyData.methods = reportInfo.methods
+      .filter(method => method.numberOfCalls > 0 && method.gasData && method.gasData.length > 0)
+      .map(method => {
+        const stats = calculateGasStatistics(method.gasData);
+        return {
+          methodName: method.methodName,
+          numberOfCalls: method.numberOfCalls,
+          ...stats,
+        };
+      });
+  } else {
+    console.warn("Warning: 'methods' property is missing or empty.");
+  }
+
+  // Filter and aggregate deployment data
+  if (reportInfo.deployments) {
+    tableReadyData.deployments = reportInfo.deployments
+      .filter(deployment => deployment.gasData && deployment.gasData.length > 0)
+      .map(deployment => {
+        const stats = calculateGasStatistics(deployment.gasData);
+        return {
+          deploymentName: deployment.deploymentName,
+          ...stats,
+        };
+      });
+  } else {
+    console.warn("Warning: 'deployments' property is missing or empty.");
+  }
+
+  // Set this filtered data as an output variable for the next step
+  console.log(`::set-output name=filtered_report::${JSON.stringify(tableReadyData)}`);
+
+  // Save the filtered report to a new JSON file for local testing
+  fs.writeFileSync('filteredGasReportOutput.json', JSON.stringify(tableReadyData, null, 2));
+
+  console.log('Successfully filtered and aggregated the gas report.');
+} catch (error) {
+  console.error(`Error occurred: ${error}`);
+  process.exit(1);
+}
