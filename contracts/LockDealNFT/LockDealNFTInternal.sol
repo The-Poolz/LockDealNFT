30aea06d8b33f655ed138f5ec291df4f853c5db2
Stan Goldin <salty87@gmail.com>
2023-09-10 12:09:40 +0300

    add check for inner id

--- contracts/LockDealNFT/LockDealNFTInternal.sol
+++ contracts/LockDealNFT/LockDealNFTInternal.sol
@@ -59,7 +59,9 @@ abstract contract LockDealNFTInternal is LockDealNFTModifiers {
             withdrawnAmount = 0;
             uint256[] memory ids = IInnerWithdraw(address(provider)).getInnerIdsArray(poolId);
             for (uint256 i = 0; i < ids.length; ++i) {
-                _withdraw(from, ids[i]);
+                uint256 id = ids[i];
+                require(ownerOf(id) == address(poolIdToProvider[poolId]), "invalid inner id");
+                _withdraw(from, id);
             }
         }
         _withdrawFromVault(poolId, withdrawnAmount, from);
