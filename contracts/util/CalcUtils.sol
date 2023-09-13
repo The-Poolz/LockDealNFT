48874b9965145f19cabf8913b5d999194d52ed09
Stan Goldin <salty87@gmail.com>
2023-09-13 12:55:18 +0300

    add safeCalcRate

--- contracts/util/CalcUtils.sol
+++ contracts/util/CalcUtils.sol
@@ -5,4 +5,27 @@ library CalcUtils {
     function calcAmount(uint256 amount, uint256 rate) internal pure returns (uint256 tokenA) {
         return (amount * rate) / 1e18;
     }
+
+    function calcRate(uint256 tokenAValue, uint256 tokenBValue) internal pure returns (uint256 rate) {
+        return (tokenAValue * 1e18) / tokenBValue;
+    }
+
+    function safeCalcRate(uint256 tokenAValue, uint256 tokenBValue) internal pure returns (uint256 rate) {
+        rate = calcRate(tokenAValue, tokenBValue);
+        uint256 reverseCalc = calcAmount(tokenAValue, rate);
+
+        // Fine-tune the rate if the reverse calculation doesn't match the original value
+        while (reverseCalc < tokenBValue) {
+            rate++;
+            reverseCalc = calcAmount(tokenAValue, rate);
+        }
+
+        // Optionally handle the case where reverseCalc > tokenBValue
+        while (reverseCalc > tokenBValue) {
+            rate--;
+            reverseCalc = calcAmount(tokenAValue, rate);
+        }
+
+        return rate;
+    }
 }
