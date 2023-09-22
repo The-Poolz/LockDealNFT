2d4ccbb8c32cd57c8bc4d9944f53f1c765cb9d51
Andrew Dmytrenko <68740472+YouStillAlive@users.noreply.github.com>
2023-09-22 16:07:50 +0300

    added new Builder struct (#316)
    * added new Builder struct
    
    * experiments with builder (#319)
    
    * experiments with builder
    
    * stack too deep fix
    
    * update .solcover.js

--- contracts/Builders/SimpleBuilder.sol
+++ contracts/Builders/SimpleBuilder.sol
@@ -3,77 +3,85 @@ pragma solidity ^0.8.0;
 
 import "../interfaces/ISimpleProvider.sol";
 import "../interfaces/ILockDealNFT.sol";
-import "@openzeppelin/contracts/utils/introspection/ERC165Checker.sol";
 import "@openzeppelin/contracts/token/ERC721/utils/ERC721Holder.sol";
+import "./BuilderModifiers.sol";
 
 /// @title SimpleBuilder contract
 /// @notice This contract is used to create mass lock deals(NFTs)
-contract SimpleBuilder is ERC721Holder {
-    ILockDealNFT public lockDealNFT;
-
+contract SimpleBuilder is ERC721Holder, BuilderModifiers {
     constructor(ILockDealNFT _nft) {
         lockDealNFT = _nft;
-    }
-
-    struct UserPool {
-        address user;
-        uint256 amount;
     }
 
     /// @notice Build mass pools
     /// @param addressParams[0] - Provider address
     /// @param addressParams[1] - Token address
-    /// @param userPools - Array of user pools
+    /// @param userData - Array of user pools
     /// @param params - Array of params. May be empty if this is DealProvider
     function buildMassPools(
         address[] calldata addressParams,
-        UserPool[] calldata userPools,
-        uint256[] calldata params
-    ) external {
-        uint256 length = userPools.length;
+        Builder calldata userData,
+        uint256[] memory params
+    ) external validParamsLength(addressParams.length, 2) notZeroAddress(addressParams[1]) {
         require(
             ERC165Checker.supportsInterface(addressParams[0], type(ISimpleProvider).interfaceId),
             "invalid provider type"
         );
-        ISimpleProvider provider = ISimpleProvider(addressParams[0]);
+        require(userData.userPools.length > 0, "invalid params length");
+        uint256 totalAmount = userData.totalAmount;
+        _notZeroAmount(totalAmount);
         address token = addressParams[1];
-        require(token != address(0x0), "invalid token address");
-        require(length > 1, "invalid userPools length");
-        uint256 totalAmount = _calcTotalAmount(userPools);
+        ISimpleProvider provider = ISimpleProvider(addressParams[0]);
+        UserPool calldata firstUserData = userData.userPools[0];
+        uint256 length = userData.userPools.length;
         // one time transfer for deacrease number transactions
-        uint256 poolId = lockDealNFT.mintAndTransfer(address(this), token, msg.sender, totalAmount, provider);
-        for (uint256 i = 0; i < length; ++i) {
-            _createNewNFT(provider, poolId, userPools[i], params);
+        params = _concatParams(firstUserData.amount, params);
+        uint256 poolId;
+        (poolId, totalAmount) = _createFirstNFT(provider, token, totalAmount, firstUserData, params);
+        for (uint256 i = 1; i < length; ) {
+            totalAmount -= _createNewNFT(provider, poolId, userData.userPools[i], params);
+            unchecked {
+                ++i;
+            }
         }
+        assert(totalAmount == 0);
+    }
+
+    function _createFirstNFT(
+        ISimpleProvider provider,
+        address token,
+        uint256 totalAmount,
+        UserPool calldata userData,
+        uint256[] memory params
+    ) internal validUserData(userData) returns (uint256 poolId, uint256 amount) {
+        poolId = lockDealNFT.mintAndTransfer(userData.user, token, msg.sender, totalAmount, provider);
+        provider.registerPool(poolId, params);
+        amount = totalAmount - userData.amount;
     }
 
     function _createNewNFT(
         ISimpleProvider provider,
         uint256 tokenPoolId,
         UserPool calldata userData,
-        uint256[] calldata params
-    ) internal {
-        require(userData.amount > 0, "invalid user amount");
-        require(userData.user != address(0x0), "invalid user address");
+        uint256[] memory params
+    ) internal validUserData(userData) returns (uint256 amount) {
+        amount = userData.amount;
         uint256 poolId = lockDealNFT.mintForProvider(userData.user, provider);
-        provider.registerPool(poolId, _concatParams(userData.amount, params));
+        params[0] = userData.amount;
+        provider.registerPool(poolId, params);
         lockDealNFT.copyVaultId(tokenPoolId, poolId);
     }
 
-    function _calcTotalAmount(UserPool[] calldata userParams) internal pure returns (uint256 totalAmount) {
-        uint256 length = userParams.length;
-        for (uint256 i = 0; i < length; ++i) {
-            totalAmount += userParams[i].amount;
-        }
-    }
-
     ///@dev if params is empty, then return [amount]
-    function _concatParams(uint amount, uint256[] calldata params) internal pure returns (uint256[] memory result) {
+    function _concatParams(uint amount, uint256[] memory params) internal pure returns (uint256[] memory result) {
         uint256 length = params.length;
         result = new uint256[](length + 1);
         result[0] = amount;
-        for (uint256 i = 0; i < length; ++i) {
+        for (uint256 i = 0; i < length; ) {
             result[i + 1] = params[i];
+            unchecked {
+                ++i;
+            }
         }
     }
 }
