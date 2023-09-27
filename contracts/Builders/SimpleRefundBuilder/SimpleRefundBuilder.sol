cdaae2f56673b5292068d1cac136a28f45901fb7
Andrew Dmytrenko <dmitrenkoae3@gmail.com>
2023-09-27 18:01:57 +0300

    fix params bug

--- contracts/Builders/SimpleRefundBuilder/SimpleRefundBuilder.sol
+++ contracts/Builders/SimpleRefundBuilder/SimpleRefundBuilder.sol
@@ -41,9 +41,9 @@ contract SimpleRefundBuilder is ERC721Holder, BuilderInternal {
         uint256 mainCoinAmount = params[0][0];
         uint256 length = userData.userPools.length;
         uint256[] memory collateralParams = params[0];
-        params[1] = _concatParams(userData.userPools[0].amount, params[1]);
+        uint256[] memory simpleParams = _concatParams(userData.userPools[0].amount, params[1]);
         uint256 poolId;
-        (poolId, totalAmount) = _createFirstNFT(provider, addressParams[1], totalAmount, userData.userPools[0], collateralParams);
+        (poolId, totalAmount) = _createFirstNFT(provider, addressParams[1], totalAmount, userData.userPools[0], simpleParams);
         uint256[] memory refundParams = new uint256[](2);
         refundParams[0] = _createCollateralProvider(mainCoin, poolId, collateralParams);
         refundParams[1] = mainCoinAmount.calcRate(userData.totalAmount);
@@ -52,7 +52,6 @@ contract SimpleRefundBuilder is ERC721Holder, BuilderInternal {
             uint256 userAmount = userData.userPools[i].amount;
             address user = userData.userPools[i].user;
             // mint refund pools for users
-            uint256[] memory simpleParams = params[1];
             uint256 refundPoolId = lockDealNFT.mintForProvider(user, refundProvider);
             totalAmount -= _createNewNFT(provider, poolId, UserPool(address(refundProvider), userAmount), simpleParams);
             refundProvider.registerPool(refundPoolId, refundParams);
