c7032a863cdd60ea709fc7df4d471544f0f3dd81
Stan Goldin <salty87@gmail.com>
2023-10-02 23:53:20 +0300

    ProviderState was red

--- contracts/SimpleProviders/Provider/ProviderState.sol
+++ contracts/SimpleProviders/Provider/ProviderState.sol
@@ -6,11 +6,11 @@ import "../../interfaces/IProvider.sol";
 
 abstract contract ProviderState is IProvider {
     ///@dev Each provider sets its own name
-    string public name;
+    string public override name;
     ILockDealNFT public lockDealNFT;
 
     ///@dev each provider decides how many parameters it needs by overriding this function
-    function currentParamsTargetLenght() public view virtual returns (uint256) {
+    function currentParamsTargetLenght() public view virtual override returns (uint256) {
         return 1;
     }
 }
