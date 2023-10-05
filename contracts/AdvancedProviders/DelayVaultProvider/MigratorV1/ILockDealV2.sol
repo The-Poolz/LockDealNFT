86e2b0c174c884f353ce3157084859310d842a91
Stan Goldin <salty87@gmail.com>
2023-10-05 15:51:57 +0300

    add ILockDealV2

---
+++ contracts/AdvancedProviders/DelayVaultProvider/MigratorV1/ILockDealV2.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+interface ILockDealV2 {
+    function CreateNewPool(
+        address _Token, //token to lock address
+        uint256 _StartTime, //Until what time the pool will start
+        uint256 _CliffTime, //Before CliffTime can't withdraw tokens
+        uint256 _FinishTime, //Until what time the pool will end
+        uint256 _StartAmount, //Total amount of the tokens to sell in the pool
+        address _Owner // Who the tokens belong to
+    ) external payable;
+}
