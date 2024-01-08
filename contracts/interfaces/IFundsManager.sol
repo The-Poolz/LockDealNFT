c0916edf8c13f2c07a135013a652ecd49df8ded6
Andrew Dmytrenko <dmitrenkoae3@gmail.com>
2024-01-08 15:46:40 +0200

    use IProvider import

--- contracts/interfaces/IFundsManager.sol
+++ contracts/interfaces/IFundsManager.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "./ISimpleProvider.sol";
+import "./IProvider.sol";
 
 interface IFundsManager is IProvider {
     function handleWithdraw(uint256 poolId, uint256 tokenAmount) external;
