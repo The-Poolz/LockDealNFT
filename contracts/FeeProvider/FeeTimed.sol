e7b10aad2abf4ede7fed4a42bf5a7e7208c4ecc4
Stan Goldin <salty87@gmail.com>
2024-01-27 22:22:33 +0200

    fixes

--- contracts/FeeProvider/FeeTimed.sol
+++ contracts/FeeProvider/FeeTimed.sol
@@ -2,9 +2,10 @@
 pragma solidity ^0.8.0;
 
 import "../SimpleProviders/TimedProvider/TimedDealProvider.sol";
+import "../interfaces/ILockDealNFT.sol"; 
 
 contract FeeTimedProvider is TimedDealProvider {
-    constructor(ILockDealNFT lockDealNFT, FeeLockProvider provider) TimedDealProvider(lockDealNFT, address provider) {
+    constructor(ILockDealNFT lockDealNFT, IProvider provider) TimedDealProvider(lockDealNFT, address provider) {
         require(provider.name() == "FeeLockProvider", "invalid provider");
     }
 
