c0b87200c15e2bc5eb52c8a68a329d51044cb8c6
Stan Goldin <salty87@gmail.com>
2024-02-01 12:26:34 +0200

    Add _ fo internal

--- contracts/util/BatchWithdraw.sol
+++ contracts/util/BatchWithdraw.sol
@@ -11,45 +11,45 @@ contract BatchWithdraw {
     }
 
     function batchWithdraw(uint256[] calldata tokenIds) external {
-        bool isApproved = getIsApproved();
+        bool isApproved = _getIsApproved();
         for (uint256 i = tokenIds.length; i > 0; --i) {
-            withdrawIfNoEmpty(isApproved, tokenIds[i - 1]);
+            _withdrawIfNoEmpty(isApproved, tokenIds[i - 1]);
         }
     }
 
     function withdrawAll() external {
-        bool isApproved = getIsApproved();
+        bool isApproved = _getIsApproved();
         for (uint256 i = lockDealNFT.balanceOf(msg.sender); i > 0; --i) {
             uint256 token = lockDealNFT.tokenOfOwnerByIndex(msg.sender, i - 1);
-            withdrawIfNoEmpty(isApproved, token);
+            _withdrawIfNoEmpty(isApproved, token);
         }
     }
 
     function withdrawAll(address[] calldata tokenFilter) external {
-        bool isApproved = getIsApproved();
+        bool isApproved = _getIsApproved();
         for (uint256 i = lockDealNFT.balanceOf(msg.sender, tokenFilter); i > 0; --i) {
             uint256 token = lockDealNFT.tokenOfOwnerByIndex(msg.sender, i - 1, tokenFilter);
-            withdrawIfNoEmpty(isApproved, token);
+            _withdrawIfNoEmpty(isApproved, token);
         }
     }
 
-    function checkData(bool isApproved, uint256 poolId) internal view {
+    function _checkData(bool isApproved, uint256 poolId) internal view {
         require(lockDealNFT.ownerOf(poolId) == msg.sender, "BatchWithdraw: not owner");
         require(isApproved || lockDealNFT.getApproved(poolId) == address(this), "BatchWithdraw: not approved");
     }
 
-    function getIsApproved() internal view returns (bool isApproved) {
+    function _getIsApproved() internal view returns (bool isApproved) {
         isApproved = lockDealNFT.isApprovedForAll(msg.sender, address(this));
     }
 
-    function withdraw(bool isApproved, uint256 poolId) internal {
-        checkData(isApproved, poolId);
+    function _withdraw(bool isApproved, uint256 poolId) internal {
+        _checkData(isApproved, poolId);
         lockDealNFT.safeTransferFrom(msg.sender, address(lockDealNFT), poolId); // transfer to lockDealNFT = withdraw
     }
 
-    function withdrawIfNoEmpty(bool isApproved, uint256 poolId) internal {
+    function _withdrawIfNoEmpty(bool isApproved, uint256 poolId) internal {
         if (lockDealNFT.getWithdrawableAmount(poolId) > 0) {
-            withdraw(isApproved, poolId);
+            _withdraw(isApproved, poolId);
         }
     }
 }
