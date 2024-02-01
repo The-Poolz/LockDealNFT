3843f4d616cfa485c8d21218eb846ed22343099d
Stan Goldin <salty87@gmail.com>
2024-02-01 13:46:01 +0200

    add batchRefund

--- contracts/util/BatchWithdraw.sol
+++ contracts/util/BatchWithdraw.sol
@@ -12,22 +12,31 @@ contract BatchWithdraw {
 
     function batchWithdraw(uint256[] calldata tokenIds) external {
         bool isApproved = _getIsApproved();
-        for (uint256 i = tokenIds.length; i > 0;) {
+        for (uint256 i = tokenIds.length; i > 0; ) {
             _withdrawIfNoEmpty(isApproved, tokenIds[--i]);
+        }
+    }
+
+    function batchRefund(uint256[] calldata tokenIds) external {
+        bool isApproved = _getIsApproved();
+        require(isApproved, "BatchWithdraw: not approved");
+        for (uint256 i = tokenIds.length; i > 0; ) {
+            uint256 token = tokenIds[--i];
+            _withdraw(isApproved, _refund(isApproved, token));
         }
     }
 
     function withdrawAll() external {
         bool isApproved = _getIsApproved();
-        for (uint256 i = lockDealNFT.balanceOf(msg.sender); i > 0;) {
-            uint256 token = lockDealNFT.tokenOfOwnerByIndex(msg.sender, --i );
+        for (uint256 i = lockDealNFT.balanceOf(msg.sender); i > 0; ) {
+            uint256 token = lockDealNFT.tokenOfOwnerByIndex(msg.sender, --i);
             _withdrawIfNoEmpty(isApproved, token);
         }
     }
 
     function withdrawAll(address[] calldata tokenFilter) external {
         bool isApproved = _getIsApproved();
-        for (uint256 i = lockDealNFT.balanceOf(msg.sender, tokenFilter); i > 0;) {
+        for (uint256 i = lockDealNFT.balanceOf(msg.sender, tokenFilter); i > 0; ) {
             uint256 token = lockDealNFT.tokenOfOwnerByIndex(msg.sender, --i, tokenFilter);
             _withdrawIfNoEmpty(isApproved, token);
         }
@@ -47,9 +56,25 @@ contract BatchWithdraw {
         lockDealNFT.safeTransferFrom(msg.sender, address(lockDealNFT), poolId); // transfer to lockDealNFT = withdraw
     }
 
+    function _refund(bool isApproved, uint256 poolId) internal returns (uint256 newPoolId) {
+        _checkData(isApproved, poolId);
+        IProvider refundProvider = _getProvider(poolId);
+        require(_isRefundProvider(refundProvider), "BatchWithdraw: must be RefundProvider");
+        newPoolId = lockDealNFT.totalSupply();
+        lockDealNFT.safeTransferFrom(msg.sender, address(refundProvider), poolId); // transfer to refundProvider = refund
+    }
+
     function _withdrawIfNoEmpty(bool isApproved, uint256 poolId) internal {
         if (lockDealNFT.getWithdrawableAmount(poolId) > 0) {
             _withdraw(isApproved, poolId);
         }
+    }
+
+    function _isRefundProvider(IProvider provider) internal view returns (bool isRefundProvider) {
+        isRefundProvider = keccak256(bytes(provider.name())) == keccak256(bytes("RefundProvider"));
+    }
+
+    function _getProvider(uint256 poolId) internal view returns (IProvider provider) {
+        provider = lockDealNFT.poolIdToProvider(poolId);
     }
 }
