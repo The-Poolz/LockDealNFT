f348001b5d6f39eb8287ec969d9d66095fdb1543
Andrew Dmytrenko <dmitrenkoae3@gmail.com>
2025-03-10 14:33:01 +0200

    update evmVersion

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -15,7 +15,7 @@ const config: HardhatUserConfig = {
       {
         version: '0.8.25',
         settings: {
-          evmVersion: 'istanbul',
+          evmVersion: 'cancun',
           optimizer: {
             enabled: true,
             runs: 200,
